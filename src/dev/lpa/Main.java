a45c88b0fbfe13fe9444ab3931d584e40e61b068
adamd <adam.d.hinton@gmail.com>
2024-04-18 10:03:52 -0400

    EE constructors and helper mtds

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,14 +1,24 @@
 package dev.lpa;
 
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class Main {
 
     private record Employee(String name, Locale locale, ZoneId zone){
 
+        public Employee(String name, String locale, String zone) {
+            this(name, Locale.forLanguageTag(locale), ZoneId.of(zone));
+        }
+
         public Employee(String name, Locale locale, String zone) {
             this(name, locale, ZoneId.of(zone));
+        }
+
+        String getDateInfo(ZonedDateTime zdt, DateTimeFormatter dtf){
+            return "%s [%s] : %s".formatted(name, zone, zdt.format(dtf.localizedBy(locale)));
         }
     }
 
